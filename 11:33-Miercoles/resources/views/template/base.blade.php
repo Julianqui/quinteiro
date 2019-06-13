57f0fc89cc9cbb0b80d2b2bb8b0a0eba557f7e9e
Julian <julianquin8@gmail.com>
2019-06-13 10:07:25 -0300

    Entregado

---
+++ 11:33-Miercoles/resources/views/template/base.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>@yield('title')</title>
+		<link rel="stylesheet" href={{ asset('/css/app.css') }}>
+		<link rel="stylesheet" href={{ asset('/css/login.css') }}>
+	</head>
+	<body>
+		@include('partials.navbar')
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					@yield('content')
+				</div>
+			</div>
+		</div>
+		<script src={{ asset('/js/app.js') }}></script>
+	</body>
+</html>
